b9947de656e7fee428c2228c9ce877ab0cb522de
Sebastian Mikkel Walther <seba7918@elev.tec.dk>
2018-12-11 12:48:38 +0100

    dwa

--- Project/Project/Sql.cs
+++ Project/Project/Sql.cs
@@ -13,7 +13,7 @@ namespace Project
     {
         //private string strCon1 = "data source=JONATHAN-BÆRBAR; database=ProjektDB; integrated security=SSPI";        
         private string strCon1 = $"data source={Environment.MachineName}; database=ProjektDB; integrated security=SSPI";
-
+        
         SqlConnection con;
         SqlDataAdapter ada;
         SqlCommand cmd;
@@ -32,7 +32,7 @@ namespace Project
                 foreach (DataRow kunde in dt.Rows)
                 {
                     Console.Write(kunde["ID"].ToString() + " | ");
-                    Console.Write(kunde["Fornavn"].ToString());
+                    Console.Write(kunde["Fornavn"].ToString() + " ");
                     Console.Write(kunde["Efternavn"].ToString() + " | ");
                     Console.Write(kunde["Adresse"].ToString() + " | ");
                     Console.Write(kunde["Alder"].ToString() + " | ");
@@ -48,7 +48,7 @@ namespace Project
             using (con = new SqlConnection(strCon1))
             {
                 con.Open();
-                ada = new SqlDataAdapter("select * from test where ID like '" + søgning + "%' " +
+                ada = new SqlDataAdapter("select * from Kunder where ID like '" + søgning + "%' " +
                 "or Fornavn like '" + søgning + "%' " +
                 "or Efternavn like '" + søgning + "%' " +
                 "or Adresse like '" + søgning + "%' " +
@@ -60,7 +60,7 @@ namespace Project
                 foreach (DataRow kunde in dt.Rows)
                 {
                     Console.Write(kunde["ID"].ToString() + " | ");
-                    Console.Write(kunde["Fornavn"].ToString());
+                    Console.Write(kunde["Fornavn"].ToString() + " ");
                     Console.Write(kunde["Efternavn"].ToString() + " | ");
                     Console.Write(kunde["Adresse"].ToString() + " | ");
                     Console.Write(kunde["Alder"].ToString() + " | ");
