d6055f05104c3f484660536558a524811948f0f2
Jonathan Grubbe Frank <jona227g@elev.tec.dk>
2018-12-11 11:04:34 +0100

    shit

--- Project/Project/Program.cs
+++ Project/Project/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,9 +13,24 @@ namespace Project
         {
             Console.WriteLine("Hovedmenu");
 
-            Console.WriteLine("Kunder: ");
-            Console.WriteLine("Biler: ");
+            Console.WriteLine("1. Kunder");
+            Console.WriteLine("2. Biler");
 
+            int menuValg = Convert.ToInt32(Console.ReadKey().Key);
+            switch (menuValg)
+            {
+                case 1:
+                    Console.WriteLine("Kunde menu");
+                    Console.ReadKey();
+                    break;
+                case 2:
+                    Console.WriteLine("Bil menu");
+                    Console.ReadKey();
+                    break;
+
+            }
+
+            Console.ReadKey();
         }
     }
 }
