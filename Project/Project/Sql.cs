4b4031dadd6d36c70f531e620fdf4f0c509d5c07
Sebastian Mikkel Walther <seba7918@elev.tec.dk>
2018-12-11 11:56:51 +0100

    ee

--- Project/Project/Sql.cs
+++ Project/Project/Sql.cs
@@ -39,5 +39,34 @@ namespace Project
                 }
             }
         }
+
+        public void KundeSøgning(string søgning)
+        {
+            dt = new DataTable();
+            using (con = new SqlConnection(strCon1))
+            {
+                con.Open();
+                ada = new SqlDataAdapter("select * from test where ID like '" + søgning + "%' " +
+                "or Fornavn like '" + søgning + "%' " +
+                "or Efternavn like '" + søgning + "%' " +
+                "or Adresse like '" + søgning + "%' " +
+                "or Alder like '" + søgning + "%' " + 
+                "or Opretdato like '" + søgning + "%'", con);
+                ada.Fill(dt);
+                Console.WriteLine("ID | Navn | Adresse | Alder | Oprettelsesdato \n");
+
+                foreach (DataRow kunde in dt.Rows)
+                {
+                    Console.Write(kunde["ID"].ToString() + " | ");
+                    Console.Write(kunde["Fornavn"].ToString());
+                    Console.Write(kunde["Efternavn"].ToString() + " | ");
+                    Console.Write(kunde["Adresse"].ToString() + " | ");
+                    Console.Write(kunde["Alder"].ToString() + " | ");
+                    Console.Write(kunde["Opretdato"].ToString());
+                    Console.WriteLine();
+                }
+            }
+
+        }
     }
 }
