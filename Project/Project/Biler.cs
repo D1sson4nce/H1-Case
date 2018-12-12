4b9047df126ba62580a50a2043d490ff38d60763
Sebastian Mikkel Walther <seba7918@elev.tec.dk>
2018-12-12 08:34:01 +0100

    awdd

--- Project/Project/Biler.cs
+++ Project/Project/Biler.cs
@@ -3,11 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Project
 {
     class Biler
     {
+        Use use = new Use();
 
+        public void BilListe()
+        {
+            use.dt = new DataTable();
+            using (use.con = new SqlConnection(use.StrCon1))
+            {
+                use.con.Open();
+                use.ada = new SqlDataAdapter("select Regnr, Mærke, Model, Årgang, Km, Brændstoftype, Fornavn, Efternavn, Biler.Opretdato from Biler join Kunder on EjerID = ID", use.con);
+                use.ada.Fill(use.dt);
+                Console.WriteLine("Registreringsnummer | Mærke | Model | Årgang | KM | Brændstoftype | Ejer \n");
+
+                foreach (DataRow bil in use.dt.Rows)
+                {
+                    Console.Write(bil["ID"].ToString() + " | ");
+                    Console.Write(bil["Fornavn"].ToString() + " ");
+                    Console.Write(bil["Efternavn"].ToString() + " | ");
+                    Console.Write(bil["Adresse"].ToString() + " | ");
+                    Console.Write(bil["Alder"].ToString() + " | ");
+                    Console.Write(bil["Opretdato"].ToString());
+                    Console.WriteLine();
+                }
+                string e = "wad";
+                foreach (DataRow bil in use.dt.Rows)
+                {
+                    if (e == bil["Regnr"].ToString())
+                    {
+
+                    }
+                }
+            }
+        }
     }
 }
