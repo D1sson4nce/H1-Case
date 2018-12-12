2fe1ca2e97e5d8aad9eb815ac08f0c35d2e5aef6
David Nielsen <david_nielsen@hotmail.dk>
2018-12-12 11:11:19 +0100

    næh

--- Project/Project/Funktioner.cs
+++ Project/Project/Funktioner.cs
@@ -9,6 +9,7 @@ namespace Project
     class Funktioner
     {
         Kunder sql = new Kunder();
+        Biler bil = new Biler();
         public void OpretKunde()
         {
             Console.Clear();
@@ -43,6 +44,48 @@ namespace Project
             }
             sql.OpretBruger(fnavn, enavn, adresse, alder);
             Program.Kundemenu();
+        }
+
+        public void OpretBil()
+        {
+            Console.Clear();
+            Console.WriteLine("Opret en ny bil");
+            Console.Write("Registreringsnummer: ");
+            string regnr = Console.ReadLine();
+            Console.Write("Bil Mærke: ");
+            string mærke = Console.ReadLine();
+            Console.Write("Bil Model: ");
+            string model = Console.ReadLine();
+            Console.Write("Årgang: ");
+            if (int.TryParse(Console.ReadLine(), out int årgang)) ;
+            else
+            {
+                OpretBil();
+            }
+            Console.Write("Kilometer: ");
+            if (double.TryParse(Console.ReadLine(), out double km)) ;
+            else
+            {
+                OpretBil();
+            }
+            Console.Write("Brændstofstype: ");
+            string brnstoftype = Console.ReadLine();
+            Console.Write("EjerID: ");
+            if (int.TryParse(Console.ReadLine(), out int ejer)) ;
+            else
+            {
+                OpretBil();
+            }
+            if (sql.FindKunde(ejer))
+            {
+                bil.OpretBil(regnr, mærke, model, årgang, km, brnstoftype, ejer);
+            }
+            else
+            {
+                Console.WriteLine("Kunden findes ikke");
+                Console.ReadKey(true);
+                Program.Bilmenu();
+            }
         }
     }
 }
