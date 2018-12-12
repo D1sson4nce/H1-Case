37dccedd1c47bfad93926d434336ceb94c662cf1
David Nielsen <david_nielsen@hotmail.dk>
2018-12-12 09:56:32 +0100

    yes

--- Project/Project/Funktioner.cs
+++ Project/Project/Funktioner.cs
@@ -14,13 +14,33 @@ namespace Project
             Console.WriteLine("Opret en ny kunde");
             Console.Write("Fornavn: ");
             string fnavn = Console.ReadLine();
+            foreach(char c in fnavn)
+            {
+                if (int.TryParse(c.ToString(), out int i))
+                {
+                    Console.Clear();
+                    OpretKunde();
+                }
+            }
             Console.Write("Efternavn: ");
             string enavn = Console.ReadLine();
+            foreach (char c in enavn)
+            {
+                if (int.TryParse(c.ToString(), out int i))
+                {
+                    Console.Clear();
+                    OpretKunde();
+                }
+            }
             Console.Write("Adresse: ");
             string adresse = Console.ReadLine();
             Console.Write("Alder: ");
-            int.TryParse(Console.ReadLine(), out int alder);
-            //int alder = Convert.ToInt32(Console.ReadLine());
+            if (int.TryParse(Console.ReadLine(), out int alder)) ;
+            else
+            {
+                Console.Clear();
+                OpretKunde();
+            }
             sql.OpretBruger(fnavn, enavn, adresse, alder);
         }
     }
