10730796ed23179e5e74c089108d64191c08ca1b
David Nielsen <david_nielsen@hotmail.dk>
2018-12-12 09:00:57 +0100

    du dum

--- Project/Project/Funktioner.cs
+++ Project/Project/Funktioner.cs
@@ -8,6 +8,20 @@ namespace Project
 {
     class Funktioner
     {
-
+        Kunder sql = new Kunder();
+        public void OpretKunde()
+        {
+            Console.WriteLine("Opret en ny kunde");
+            Console.Write("Fornavn: ");
+            string fnavn = Console.ReadLine();
+            Console.Write("Efternavn: ");
+            string enavn = Console.ReadLine();
+            Console.Write("Adresse: ");
+            string adresse = Console.ReadLine();
+            Console.Write("Alder: ");
+            int.TryParse(Console.ReadLine(), out int alder);
+            //int alder = Convert.ToInt32(Console.ReadLine());
+            sql.OpretBruger(fnavn, enavn, adresse, alder);
+        }
     }
 }
